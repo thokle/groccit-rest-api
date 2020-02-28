e0e43258d48d8ca03ff0508592abe1a275370fd5
Thomas Kleist <thomas@kleist-it.com>
2020-02-28 21:15:43 +0100

    new contrpóller

---
+++ controllers/TestPurphaseController.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace groccit_rest_api.controllers
+{
+    public class TestPurphaseController: NancyModule 
+    {
+        public TestPurphaseController()
+        {
+            Get("/testpurphase", u => {
+                return "Hello";
+            });
+        }
+    }
+}
