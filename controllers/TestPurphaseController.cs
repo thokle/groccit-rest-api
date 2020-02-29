238f577d29f6b60b48263ae91242bbfb806ce816
Thomas Kleist <thomas@kleist-it.com>
2020-02-29 19:13:17 +0100

    save and get works now

--- controllers/TestPurphaseController.cs
+++ controllers/TestPurphaseController.cs
@@ -3,16 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using data_soruce_mil.services;
+using data_soruce_mil.models;
+using Nancy.ModelBinding;
+using Newtonsoft.Json;
 
 namespace groccit_rest_api.controllers
 {
     public class TestPurphaseController: NancyModule 
     {
-        public TestPurphaseController()
+        public TestPurphaseController(TestPurhaseIntentionService testPurhaseIntentionService)
         {
-            Get("/testpurphase", u => {
-                return "Hello";
+            Get("/testPurphase/all", u => {
+              
+             var res=  JsonConvert.SerializeObject(testPurhaseIntentionService.GetTestPurchaseIntentions());
+                return res;
             });
+            Post("/testPurphase/create", c =>
+            {
+             var test =    this.Bind<TestPurchaseIntention>();
+                var res = testPurhaseIntentionService.CreateTestPurchaseIntention(test);
+            
+                return "Oprettet ";
+            }); 
+         
         }
     }
 }
